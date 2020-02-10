bc6232175bb52609f933e1cb8df8487ed983d004
Ashleigh Maund <ashleigh.maund@accedo.tv>
2020-02-10 16:37:39 +0000

    added photon components and styles

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ const {app, BrowserWindow} = electron;
 app.on('ready', () => {
 	let win = new BrowserWindow({});
 	// TODO find a way to add option to open with dev tools using npm
-	// win.webContents.openDevTools();
+	win.webContents.openDevTools();
 	win.loadFile('index.html');
 });
