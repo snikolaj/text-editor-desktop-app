bd5cec49df0beb6f6d3adc20f82d1f6135080e46
Ashleigh Maund <ashleigh.maund@accedo.tv>
2020-02-10 16:00:03 +0000

    adding photon css and fonts and updating readme and index.html

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ README
 Text Editor
 -----------
 
+Made using Electron, Photon UI Kit
+
 To install packages, run `npm install` from command line.
 
 To start app, run `npm start` from command line.
--- main.js
+++ main.js
@@ -3,5 +3,7 @@ const {app, BrowserWindow} = electron;
 
 app.on('ready', () => {
 	let win = new BrowserWindow({});
+	// TODO find a way to add option to open with dev tools using npm
+	// win.webContents.openDevTools();
 	win.loadFile('index.html');
 });
