172a9ccbc1d747b83f5a94a74a272430fe62d2ae
Ashleigh Maund <ashleigh.maund@accedo.tv>
2020-03-04 16:15:06 +0000

    adding ability to save using ipcMain and ipcRenderer

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const electron = require('electron');
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, ipcMain} = electron;
 
 app.on('ready', () => {
-	let win = new BrowserWindow({});
+	let win = new BrowserWindow({
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
 	// TODO find a way to add option to open with dev tools using npm
 	win.webContents.openDevTools();
 	win.loadFile('index.html');
+});
+
+ipcMain.on('save', (event, text) => {
+	console.log('ipcMain save', event, text)
 });
